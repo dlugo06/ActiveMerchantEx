6d5325d79c52b218ecf44a027242a5f85d0a933d
Diego Lugo <diego.lugo.06@gmail.com>
2015-12-15 16:39:45 -0500

    Change root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   resources :stripe_charges
   resources :braintree, only: [:new, :create]
-  get '/landing' => 'payments#landing'
+
+  root 'payments#landing'
 
 end
