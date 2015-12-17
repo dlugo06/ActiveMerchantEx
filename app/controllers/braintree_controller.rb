127d2eac8b6f677247a6f81859d78722f3c39009
Diego Lugo <diego.lugo.06@gmail.com>
2015-12-16 20:18:47 -0500

    Card interface taking input and offline validation. Not pulling fields

--- app/controllers/braintree_controller.rb
+++ app/controllers/braintree_controller.rb
@@ -1,5 +1,4 @@
 class BraintreeController < ApplicationController
-  require 'activemerchant'
 
   def new
     gon.client_token = generate_client_token
@@ -16,39 +15,6 @@ class BraintreeController < ApplicationController
     end
   end
 
-  def active_merchant
-    ActiveMerchant::Billing::Base.mode = :test
-
-    #Paypal Gateway
-    # gateway = ActiveMerchant::Billing::PaypalGateway.new(
-    #   :login => ENV["PAYPAL_LOGIN"],
-    #   :password => ENV["PAYPAL_PASSWORD"],
-    #   :signature => ENV["PAYPAL_SIGNATURE"]
-    # )
-
-    gateway = ActiveMerchant::Billing::TrustCommerceGateway.new(:login => 'TestMerchant', :password => 'password')
-
-    credit_card = ActiveMerchant::Billing::CreditCard.new(
-      :brand              => "visa",
-      :number             => "4024007148673576",
-      :verification_value => "123",
-      :month              => 1,
-      :year               => Time.now.year+1,
-      :first_name         => "Ryan",
-      :last_name          => "Bates"
-    )
-
-    if credit_card.validate.empty?
-      response = gateway.purchase(1000, credit_card, :ip => "127.0.0.1")
-      if response.success?
-        redirect_to root_path, alert: "ActiveMerchant Purchase complete!"
-      else
-        redirect_to root_path, alert: "Error: #{response.message}"
-      end
-    else
-      redirect_to root_path, alert: "Error: credit card is not valid. #{credit_card.errors.full_messages.join('. ')}"
-    end
-  end
 
   private
   def generate_client_token
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   resources :stripe_charges
   resources :braintree, only: [:new, :create]
-  get '/activemerchant' => 'braintree#active_merchant'
+  get '/activemerchant' => 'payments#active_merchant'
+  post '/activemerchant' => 'payments#purchase'
   root 'payments#landing'
 
 end
