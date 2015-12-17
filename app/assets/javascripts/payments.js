b63329b2f5e2bdec28ebfbca3c53891cf8b7ac65
Diego Lugo <diego.lugo.06@gmail.com>
2015-12-16 22:55:07 -0500

    Move js away from controller

--- app/assets/javascripts/payments.js
+++ app/assets/javascripts/payments.js
@@ -1,15 +1,3 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
-
-
-
-function newCard() {
-  var card = new Card({
-    form: 'form',
-    container: '.card',
-    debug: true
-  });
-}
-
-$(window).load(newCard);
