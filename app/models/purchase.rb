e3fa556c62c6cac60d565a626e170e6575b8b6c0
Diego Lugo <diego.lugo.06@gmail.com>
2015-12-17 22:08:23 -0500

    Move activemerchant actions to model

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,2 +1,19 @@
 class Purchase < ActiveRecord::Base
+  require 'activemerchant'
+
+  def active_purchase(card)
+    ActiveMerchant::Billing::Base.mode = :test
+
+    # Paypal Gateway
+    # gateway = ActiveMerchant::Billing::PaypalGateway.new(
+    #   :login => ENV["PAYPAL_LOGIN"],
+    #   :password => ENV["PAYPAL_PASSWORD"],
+    #   :signature => ENV["PAYPAL_SIGNATURE"]
+    # )
+
+    gateway = ActiveMerchant::Billing::TrustCommerceGateway.new(:login => 'TestMerchant', :password => 'password')
+
+    response = gateway.purchase(1000, card, :ip => "127.0.0.1")
+    raise
+  end
 end
