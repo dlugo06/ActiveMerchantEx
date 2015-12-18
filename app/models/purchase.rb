740f3733a513b2158555b3c85885f6bd15e7c6d2
Diego Lugo <diego.lugo.06@gmail.com>
2015-12-17 23:09:23 -0500

    Set example to Stripe

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -4,15 +4,15 @@ class Purchase < ActiveRecord::Base
   def active_purchase(card)
     ActiveMerchant::Billing::Base.mode = :test
 
-    # Paypal Gateway
-    gateway = ActiveMerchant::Billing::PaypalGateway.new(
-      :login => ENV["PAYPAL_LOGIN"],
-      :password => ENV["PAYPAL_PASSWORD"],
-      :signature => ENV["PAYPAL_SIGNATURE"]
-    )
+    # # Paypal Gateway
+    # gateway = ActiveMerchant::Billing::PaypalGateway.new(
+    #   :login => ENV["PAYPAL_LOGIN"],
+    #   :password => ENV["PAYPAL_PASSWORD"],
+    #   :signature => ENV["PAYPAL_SIGNATURE"]
+    # )
 
     # #Stripe Gateway
-    # gateway = ActiveMerchant::Billing::StripeGateway.new(:login => ENV['SECRET_KEY'])
+    gateway = ActiveMerchant::Billing::StripeGateway.new(:login => ENV['SECRET_KEY'])
 
     # gateway = ActiveMerchant::Billing::TrustCommerceGateway.new(:login => 'TestMerchant', :password => 'password')
 
